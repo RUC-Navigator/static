a78d08b0068f32a5440e58c6d359ffd556f4db70
Abuccts <abuccts@gmail.com>
2015-11-08 16:20:48 +0800

    init web page

---
+++ recorder.js
@@ -0,0 +1,229 @@
+// variables
+var leftchannel = [];
+var rightchannel = [];
+var recorder = null;
+var recording = false;
+var recordingLength = 0;
+var volume = null;
+var audioInput = null;
+var sampleRate = null;
+var audioContext = null;
+var context = null;
+var outputElement = document.getElementById('output');
+var outputString;
+
+// feature detection
+if (!navigator.getUserMedia)
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+                  navigator.mozGetUserMedia || navigator.msGetUserMedia;
+
+if (navigator.getUserMedia){
+    navigator.getUserMedia({audio:true}, success, function(e) {
+    alert('Error capturing audio.');
+    });
+} else alert('getUserMedia not supported in this browser.');
+
+
+function startRecord() {
+    // if micro is pressed, we start recording
+    recording = true;
+    // reset the buffers for the new recording
+    leftchannel.length = rightchannel.length = 0;
+    recordingLength = 0;
+    outputElement.innerHTML = 'Recording now ...';
+    // if S is pressed, we stop the recording and package the WAV file
+}
+
+function stopRecord() {
+    // we stop recording
+    recording = false;
+
+    outputElement.innerHTML = 'Uploadinging wav file ...';
+	
+    // we flat the left and right channels down
+    var leftBuffer = mergeBuffers ( leftchannel, recordingLength );
+    var rightBuffer = mergeBuffers ( rightchannel, recordingLength );
+    // we interleave both channels together
+    var interleaved = interleave ( leftBuffer, rightBuffer );
+
+    // we create our wav file
+    var buffer = new ArrayBuffer(44 + interleaved.length * 2);
+    var view = new DataView(buffer);
+
+    // RIFF chunk descriptor
+    writeUTFBytes(view, 0, 'RIFF');
+    view.setUint32(4, 44 + interleaved.length * 2, true);
+    writeUTFBytes(view, 8, 'WAVE');
+    // FMT sub-chunk
+    writeUTFBytes(view, 12, 'fmt ');
+    view.setUint32(16, 16, true);
+    view.setUint16(20, 1, true);
+    // stereo (2 channels)
+    view.setUint16(22, 2, true);
+    view.setUint32(24, sampleRate, true);
+    view.setUint32(28, sampleRate * 4, true);
+    view.setUint16(32, 4, true);
+    view.setUint16(34, 16, true);
+    // data sub-chunk
+    writeUTFBytes(view, 36, 'data');
+    view.setUint32(40, interleaved.length * 2, true);
+
+    // write the PCM samples
+    var lng = interleaved.length;
+    var index = 44;
+    var volume = 1;
+    for (var i = 0; i < lng; i++){
+        view.setInt16(index, interleaved[i] * (0x7FFF * volume), true);
+        index += 2;
+    }
+
+    // our final binary blob
+    var blob = new Blob ( [ view ], { type : 'audio/wav' } );
+
+
+    var filename = 'audio_recording_' + new Date().getTime() + '.wav';
+
+	
+
+    // let's save it locally
+    // outputElement.innerHTML = 'Handing off the file now...';
+    var url = (window.URL || window.webkitURL).createObjectURL(blob);
+
+
+	var li = document.createElement('li');
+	var au = document.createElement('audio');
+	var hf = document.createElement('a');
+
+    uploadAudio(blob, filename, $(li));
+
+	au.controls = true;
+	au.src = url;
+	hf.href = url;
+	hf.download = filename;
+	hf.innerHTML = "download wav file";
+	li.appendChild(au);
+	li.appendChild(hf);
+	recordingslist.appendChild(li);
+    $(li).addClass('li-send pull-left');
+	li.scrollIntoView();
+	
+	outputElement.innerHTML = 'Press to start speaking';
+}
+
+
+function uploadAudio(wavData, filename, $li){
+	var reader = new FileReader();
+	reader.onload = function(event){
+		var fd = new FormData();
+		var wavName = encodeURIComponent(filename);
+
+		fd.append('fname', wavName);
+		fd.append('data', event.target.result);
+		$.ajax({
+			type: 'POST',
+			url: '/postaudio',
+			data: fd,
+			processData: false,
+			contentType: false,
+            success: function (res) {
+                if (res.status === 0) {
+                    var id = res.id;
+                    var itv = setInterval(function () {
+                        $.get('/getresult?id=' + id, function (res) {
+                            if (res.status === 0) {
+                                clearInterval(itv);
+								$('<span> ' + res.pre.split('$')[2] + '</span>').appendTo($li);
+                                var $newli = $('<li class="li-reply pull-right"></li>');
+                                $newli.insertAfter($li);
+                                var audio = document.createElement("audio");
+                                audio.src = res.path;
+                                $(audio).appendTo($newli);
+                                $('<a href="' + res.path + '" target="_blank">下载文件</a>').appendTo($newli);
+                                $('<span></span>').addClass('txt').text(res.txt).appendTo($newli);
+                            }
+                        });
+                    }, 1000);
+                }
+            },
+            error: function () {
+                console.log(arguments);
+            }
+		});
+	};
+	reader.readAsDataURL(wavData);
+}
+
+function interleave(leftChannel, rightChannel){
+  var length = leftChannel.length + rightChannel.length;
+  var result = new Float32Array(length);
+
+  var inputIndex = 0;
+
+  for (var index = 0; index < length; ){
+    result[index++] = leftChannel[inputIndex];
+    result[index++] = rightChannel[inputIndex];
+    inputIndex++;
+  }
+  return result;
+}
+
+function mergeBuffers(channelBuffer, recordingLength){
+  var result = new Float32Array(recordingLength);
+  var offset = 0;
+  var lng = channelBuffer.length;
+  for (var i = 0; i < lng; i++){
+    var buffer = channelBuffer[i];
+    result.set(buffer, offset);
+    offset += buffer.length;
+  }
+  return result;
+}
+
+function writeUTFBytes(view, offset, string){
+  var lng = string.length;
+  for (var i = 0; i < lng; i++){
+    view.setUint8(offset + i, string.charCodeAt(i));
+  }
+}
+
+function success(e){
+    // creates the audio context
+    audioContext = window.AudioContext || window.webkitAudioContext;
+    context = new audioContext();
+
+	// we query the context sample rate (varies depending on platforms)
+    sampleRate = context.sampleRate;
+
+    console.log('succcess');
+
+    // creates a gain node
+    volume = context.createGain();
+
+    // creates an audio node from the microphone incoming stream
+    audioInput = context.createMediaStreamSource(e);
+
+    // connect the stream to the gain node
+    audioInput.connect(volume);
+
+    /* From the spec: This value controls how frequently the audioprocess event is
+    dispatched and how many sample-frames need to be processed each call.
+    Lower values for buffer size will result in a lower (better) latency.
+    Higher values will be necessary to avoid audio breakup and glitches */
+    var bufferSize = 2048;
+    recorder = context.createScriptProcessor(bufferSize, 2, 2);
+
+    recorder.onaudioprocess = function(e){
+        if (!recording) return;
+        var left = e.inputBuffer.getChannelData (0);
+        var right = e.inputBuffer.getChannelData (1);
+        // we clone the samples
+        leftchannel.push (new Float32Array (left));
+        rightchannel.push (new Float32Array (right));
+        recordingLength += bufferSize;
+        console.log('recording');
+    }
+
+    // we connect the recorder
+    volume.connect (recorder);
+    recorder.connect (context.destination);
+}
